258dbea14b4df5a201e13aef136555411aa501f7
Matheus Henrique <amhenrique157@gmail.com>
2024-09-06 15:30:20 -0300

    atializacao das urls

--- projeto_pos_venda/projeto_pos_venda/urls.py
+++ projeto_pos_venda/projeto_pos_venda/urls.py
@@ -1,12 +1,9 @@
-
 from django.contrib import admin
 from django.urls import include, path
 from app_cad_usuarios import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
-   
     #sequencia correta
     # rota, views, nome de referenci
     path('usuarios/', views.usuarios, name='listagem_usuario'),
@@ -18,10 +15,4 @@ urlpatterns = [
     path('login/', views.logout_view, name='logout'),
     path('clientes/', views.clienteslista, name='clientes'),
     path('clientescadastro/', views.clientecadastro, name='clientecadastro'),
-    
-      #adicionar função de login
-    
-   
-    #usuarios.com/usuarios
-    
 ]
